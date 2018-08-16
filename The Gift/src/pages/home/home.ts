8b85d46b39bf3d7f409bbe192f6dfc2641068e16
felipecisotto <felipecisotto@hotmail.com>
2018-08-16 20:56:56 -0300

    Ajustes tela do bot

--- The Gift/src/pages/home/home.ts
+++ The Gift/src/pages/home/home.ts
@@ -10,38 +10,47 @@ import { ChatProvider } from './../../providers/chat/chat';
 })
 export class HomePage {
   messages = [];
-  nickname = "Felipe";
+  nickname = "Me";
   chats = [];
   context = {};
   chat_input;
-  
+
   constructor(public navCtrl: NavController, private chatProvider: ChatProvider) {
     this.talk(null)
   }
 
-  send(msg){
-    var message = {text: msg, from: "Felipe", created: new Date()}; 
-    this.chat_input = "";
-    this.chats.push(message);     
-    this.talk(msg); 
+  send(msg) {
+    if (msg != null) {
+      var message = { text: msg, from: "Me", created: new Date(),hasImage:false };
+      this.chat_input = "";
+      this.chats.push(message);
+      this.talk(msg);
+    }
   }
 
   talk(msg) {
-    this.chatProvider.chat(this.context,msg)
-    .then((result: any) => {
-      result.data.output.text.forEach(element => {
-        var message = {text: element, from: "Watson", created: new Date()}; 
-        this.chats.push(message);     
+    this.chatProvider.chat(this.context, msg)
+      .then((result: any) => {
+        result.data.output.text.forEach((element,index,array) => {
+          var hasImage = false;
+          if (index === (array.length-1)){
+            var hasImage = true;
+            
+          }
+          var message = { text: element, from: "Watson", created: new Date(),hasImage: hasImage };
+          console.log(message.hasImage);
+          this.chats.push(message);
+
+        });
+        this.context = result.data.context;
+
+      })
+      .catch((error: any) => {
+
       });
-      this.context = result.data.context;
-      
-    })
-    .catch((error: any) => {
-      
-    });
 
 
-}
+  }
 
 
 
